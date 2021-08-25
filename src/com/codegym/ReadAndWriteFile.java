ca765b4c6407c2b51194dc993b7abaacd29e3b51
huongtran99 <huongtrannd1999@gmail.com>
2021-08-25 21:02:11 +0700

    first commit

---
+++ src/com/codegym/ReadAndWriteFile.java
@@ -0,0 +1,57 @@
+package com.codegym;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadAndWriteFile {
+
+    public static void main(String[] args) {
+        ReadAndWriteFile readAndWriteFile = new ReadAndWriteFile();
+        List<Integer> numbers = readAndWriteFile.readFile("numbers.txt");
+        int maxValue = findMax(numbers);
+        readAndWriteFile.writeFile("result.txt", maxValue);
+    }
+
+    public static int findMax(List<Integer> numbers) {
+        int max = numbers.get(0);
+        for (Integer number : numbers) {
+            if (max < number) {
+                max = number;
+            }
+        }
+        return max;
+    }
+
+    public void writeFile(String filePath, int max){
+        try {
+            FileWriter writer = new FileWriter(filePath, true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.write("Giá trị lớn nhất là: " + max + "\n");
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Integer> readFile(String filePath){
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            File file = new File(filePath);
+
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                numbers.add(Integer.parseInt(line));
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("Fie không tồn tại or nội dung có lỗi!");
+        }
+        return numbers;
+    }
+}
